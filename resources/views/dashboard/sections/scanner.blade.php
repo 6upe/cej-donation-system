d2333b0e8a34fcd7761b8d3265ec79dcca4b59a2
6upe <katongobupe444@gmail.com>
2026-04-21 17:43:29 +0200

    Product Status

--- resources/views/dashboard/sections/scanner.blade.php
+++ resources/views/dashboard/sections/scanner.blade.php
@@ -74,7 +74,7 @@ function fetchParticipant(id) {
                         <p><strong>Name:</strong> ${data.data.name}</p>
                         <p><strong>Email:</strong> ${data.data.email}</p>
                         <p><strong>Package:</strong> ${data.data.ticket_package}</p>
-                        <p><strong>Status:</strong> ${(data.data.product_status || []).join(', ')}</p>
+                        <p><strong>Status:</strong> ${data.data.product_status.join(', ')}</p>
 
                         <select id="status" class="form-select mb-3" multiple>
                             <option value="registered">Registered</option>
